29983785daa37d9d9d26c342b4f57a5584412df0
js-eng <80029504+js-eng@users.noreply.github.com>
2021-03-10 22:08:13 +0530

    Add files via upload
    DSA - Assignement 1

---
+++ Assignment 1 - Juhi.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+x = int(input("Enter x value"))
+y = int(input("Enter y value"))
+
+z = pow(x,y)
+
+print(z)
+
+
+# In[10]:
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+for x in numbers:
+    if (x%3 == 0) :
+        print(x)
+    else:
+        print("Not divisible by 3")
+
+
+# In[ ]:
+
+
+
+
